720cf7d76a0b6b8a90601b5e40caf7e7e670e84c
Alice Chen <alicexinyun@163.com>
2016-11-04 01:09:05 -0400

    game.ml and main.ml related modifications

---
+++ game.mli
@@ -0,0 +1,7 @@
+open Data
+open Map
+open Ai
+open User
+
+(* [main n d] is the main entry point from outside this module to initialize a game with [n] AI bots and a difficulty level of [d] and start playing it. *)
+val main : int -> int -> unit
