0af37af6d0f043d68186dac3b2bb1e7d11df7d0a
Zhao, David <dfz8@cornell.edu>
2016-11-06 22:28:16 -0500

    removed module headers

--- ai.mli
+++ ai.mli
@@ -1,56 +1,51 @@
 open Data
 
-(* [Ai] is the module that handles interaction with an AI. *)
-module type Ai = sig 
+(* [t] is the data type representing the Ai's state and goals *)
+type t
 
-  (* [t] is the data type representing the Ai's state and goals *)
-  type t
+(***********************************************
+ * Utility methods
+ ***********************************************)
 
-  (***********************************************
-   * Utility methods
-   ***********************************************)
+(* [init p d hand] creates and returns an AI data structure that represents an
+ * AI playing character [p] on difficulty [d], with starting hand [hand].
+ * Returns: the initialized setup for an AI.
+ *)
+val init: string -> Difficulty -> hand -> t
 
-  (* [init p d hand] creates and returns an AI data structure that represents an
-   * AI playing character [p] on difficulty [d], with starting hand [hand].
-   * Returns: the initialized setup for an AI.
-   *)
-  val init: string -> Difficulty -> hand -> t
+(* [get_ai state p] 
+ * Returns: the AI data structure for the AI playing character [p] 
+ *)
+val get_ai: state -> string -> t
 
-  (* [get_ai state p] 
-   * Returns: the AI data structure for the AI playing character [p] 
-   *)
-  val get_ai: state -> string -> t
+(* [get_difficulty ai] 
+ * Returns: the difficulty of the AI [ai]. 
+ *)
+val get_difficulty: t -> Difficulty
 
-  (* [get_difficulty ai] 
-   * Returns: the difficulty of the AI [ai]. 
-   *)
-  val get_difficulty: t -> Difficulty
-
-  (* [still_in_game ai]
-   * Returns: [true] iff the AI [ai] is still in the game. (If you're out you 
-   * can still prove suggestions wrong) 
-   *)
-  val still_in_game: t -> bool
+(* [still_in_game ai]
+ * Returns: [true] iff the AI [ai] is still in the game. (If you're out you 
+ * can still prove suggestions wrong) 
+ *)
+val still_in_game: t -> bool
 
 
-  (************************************************
-   * Methods for interacting with game state
-   ************************************************)
+(************************************************
+ * Methods for interacting with game state
+ ************************************************)
 
-  (* [step state p] peforms a turn for player [p] (an AI player). This involves:
-   *   - defining and setting goals by processing knowledge from suggestions and
-   *     making deductions about other players' turns.
-   *   - moving the AI around the map
-   *   - forming and making suggestions/accusations
-   * Returns: an updated game state.
-   *)
-  val step: state -> string -> state
+(* [step state p] peforms a turn for player [p] (an AI player). This involves:
+ *   - defining and setting goals by processing knowledge from suggestions and
+ *     making deductions about other players' turns.
+ *   - moving the AI around the map
+ *   - forming and making suggestions/accusations
+ * Returns: an updated game state.
+ *)
+val step: state -> string -> state
 
-  (* [reveal_card ai guess] figures out which card to reveal in response
-   * to a suggestion/accusation [guess].
-   * Returns: [Some c] where [c] is a card that [ai] can reveal. Or, if [ai] has
-   * none of the cards in [guess], then it will return [None]. 
-   *)
-  val reveal_card: t -> card list -> card option
-
-end
+(* [reveal_card ai guess] figures out which card to reveal in response
+ * to a suggestion/accusation [guess].
+ * Returns: [Some c] where [c] is a card that [ai] can reveal. Or, if [ai] has
+ * none of the cards in [guess], then it will return [None]. 
+ *)
+val reveal_card: t -> card list -> card option
