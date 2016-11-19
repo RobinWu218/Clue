072a8f930c2ee262552ba1b0b40add8d5dd35792
Zhao, David <dfz8@cornell.edu>
2016-11-19 13:10:35 -0500

    edited reader

--- reader.ml
+++ reader.ml
@@ -1,39 +1,8 @@
 open Yojson.Basic.Util
 open Data
 
-(* The type of source file *)
-type j = Yojson.Basic.json
-
-(* Methods of reading json files and loading into local types*)
-
-(* [make_prof j] is the professor returned by a json *)
-let make_prof j =
-  failwith "unimplemented"
-
-(* [make_building j] is the building returned by a json *)
-let make_building j = 
-  failwith "unimplemented"
-
-(* [make_language j] is the language returned by a json *)
-let make_language j = 
-  failwith "unimplemented"
-
-(* [make_card j] is the card returned by a json *)
-let make_card j = 
-  failwith "unimplemented"
-
-(* [make_case_file j] is the case_file returned by a json *)
-let make_case_file j =
-  failwith "unimplemented"
-
 (* [make_map] parses the json file storing information about the map and
  * converts it into a [map].
  * Returns: a [map] with only information about the map (no player info) *)
  let make_map () = 
-  failwith "unimplemented"
-
-(* [make_state j] is the initial state returned by a json. This 
- * state includes the current map situation and player's and 
- * ais' information *)
- let make_state j = 
   failwith "unimplemented"
--- reader.mli
+++ reader.mli
@@ -1,31 +1,7 @@
 open Yojson.Basic.Util
 open Data
 
-(* The type of source file *)
-type j = Yojson.Basic.json
-
-(* Methods of reading json files and loading into local types*)
-(* [make_prof j] is the professor returned by a json *)
-val make_prof : j -> prof
-
-(* [make_building j] is the building returned by a json *)
-val make_building : j -> building
-
-(* [make_language j] is the language returned by a json *)
-val make_language : j -> language
-
-(* [make_card j] is the card returned by a json *)
-val make_card : j -> card 
-
-(* [make_case_file j] is the case_file returned by a json *)
-val make_case_file : j -> case_file 
-
 (* [make_map] parses the json file storing information about the map and
  * converts it into a [map].
  * Returns: a [map] with only information about the map (no player info) *)
 val make_map: unit -> map
-
-(* [make_state j] is the initial state returned by a json. This 
- * state includes the current map situation and player's and 
- * ais' information *)
-val make_state : j -> state
