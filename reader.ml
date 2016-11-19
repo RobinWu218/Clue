5bfbb2907a5875cf68097c35279127019bf8add0
Zhao, David <dfz8@cornell.edu>
2016-11-19 03:55:17 -0500

    skeletons for map and reader

--- reader.ml
+++ reader.ml
@@ -5,27 +5,35 @@ open Data
 type j = Yojson.Basic.json
 
 (* Methods of reading json files and loading into local types*)
+
 (* [make_prof j] is the professor returned by a json *)
-val make_prof : j -> prof
+let make_prof j =
+  failwith "unimplemented"
 
 (* [make_building j] is the building returned by a json *)
-val make_building : j -> building
+let make_building j = 
+  failwith "unimplemented"
 
 (* [make_language j] is the language returned by a json *)
-val make_language : j -> language
+let make_language j = 
+  failwith "unimplemented"
 
 (* [make_card j] is the card returned by a json *)
-val make_card : j -> card 
+let make_card j = 
+  failwith "unimplemented"
 
 (* [make_case_file j] is the case_file returned by a json *)
-val make_case_file : j -> case_file 
+let make_case_file j =
+  failwith "unimplemented"
 
 (* [make_map] parses the json file storing information about the map and
  * converts it into a [map].
  * Returns: a [map] with only information about the map (no player info) *)
-val make_map: unit -> map
+ let make_map () = 
+  failwith "unimplemented"
 
 (* [make_state j] is the initial state returned by a json. This 
  * state includes the current map situation and player's and 
  * ais' information *)
-val make_state : j -> state
+ let make_state j = 
+  failwith "unimplemented"
