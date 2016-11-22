09cc93a7ec232c9c8837e7aaa15bfd2caaf2ad23
Zhao, David <dfz8@cornell.edu>
2016-11-22 17:01:36 -0500

    added gitignore

--- _build/reader.mli
+++
@@ -1,7 +0,0 @@
-open Yojson.Basic.Util
-open Data
-
-(* [make_map] parses the json file storing information about the map and
- * converts it into a [map].
- * Returns: a [map] with only information about the map (no player info) *)
-val make_map: unit -> map
