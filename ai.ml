2702e295b9fd666bef3853c3c8a79dc004925fb2
Karen Fang <kf288@cornell.edu>
2016-11-27 22:28:37 -0500

    ai step almost done

--- ai.ml
+++ ai.ml
@@ -13,7 +13,15 @@ type t = ai
  * AI playing character [p] on difficulty [d], with starting hand [hand].
  * Returns: the initialized setup for an AI.
  *)
-let init p d hand = {
+let init p d hand =
+  let possible = [Prof "Bracy"; Prof "Clarkson";Prof "Fan";Prof "Gries";
+  Prof "Halpern";Prof "White";Building "Baker";Building "Carpenter";
+  Building "Duffield";Building "Gates";Building "Klarman";Building "Olin";
+  Building "Phillips";Building "Rhodes";Building "Statler";Language "Bash";
+  Language "C";Language "Java";Language "MATLAB";Language "OCaml";
+  Language "Python"]
+in
+  {
   character =p;
   hand=hand;
   was_moved=false;
@@ -21,9 +29,8 @@ let init p d hand = {
   difficulty=d;
   destination=None;
   known_cards=hand;
-  possible_cards=[];
+  possible_cards=possible;
   past_guesses=[]
-  (*(case_file * prof * (prof option)) list;*)
 }
 
 let rec help_get_ai ais p=
@@ -58,11 +65,11 @@ let still_in_game ai =
 
 (* [update_ai ai player guess player2] updates the knowledge of [ai] when
  * [player] makes a [guess] that got disproved by [player2]. *)
-let update_ai ai player guess player2 =
+let update_ai ai prof1 guess prof2 =
   (*TODO: if the ai has two of the three cards in its hand and the guess is
    * disproved by someone else, then that third card also becomes a known card.*)
 {
-  character =p;
+  character = ai.character;
   hand=ai.hand;
   was_moved=ai.was_moved;
   is_in_game=ai.is_in_game;
@@ -70,7 +77,7 @@ let update_ai ai player guess player2 =
   destination=ai.destination;
   known_cards=ai.known_cards;
   possible_cards=ai.possible_cards;
-  past_guesses=ai.past_guesses^(guess, player, player2)
+  past_guesses=(guess, prof1, prof2)::ai.past_guesses
 }
 
 (*[easy_helper_who possible] takes a list of possible cards and returns the
@@ -78,7 +85,7 @@ let update_ai ai player guess player2 =
 let rec easy_helper_who possible =
               match possible with
               |[]   -> failwith "there are no possible professors"
-              |h::t -> if (((Data.int_of_card h)>-1)||((Data.int_of_card h)<6))
+              |h::t -> if (((Data.int_of_card h)> (-1))||((Data.int_of_card h)<6))
                then h else easy_helper_who t
 
 (*[easy_helper_who possible] takes a list of possible cards and returns the
@@ -128,54 +135,67 @@ let easy_want_to_accuse possible_cards ai =
 
 (*[updated_state_map state new_map] returns the updated state with new_map.
  * Nothing else in state is changed*)
-let updated_state_map state new_map= {
-  counter=state.counter;
-  game_complete= game_complete;
-  map=new_map;
-  user=state.user;
-  ais=state.ais;
-  fact_file=state.fact_file;
-  dictionary=state.dictionary;
-}
+let updated_state_map state new_map=
+{state with map=new_map}
 
-(*[help_disprove state guess] returns Some card that some player disproved the
- * guess with or returns None if the guess is not disproved. *)
-let rec help_disprove players guess=
+(*[character_to_ai] prof ai_list] takes in the prof and a list of ais and returns
+ * the ai that corresponds to that professor.
+ * requires: the prof must match with a ai that is currently in the game*)
+let rec character_to_ai prof ai_list=
+  match ai_list with
+  |[]    -> failwith "should not occur"
+  |h::t  ->if h.character=prof then h else character_to_ai prof t
+
+(*[help_disprove players state guess] takes in [players], a prof list of the
+ * current players, and a state and returns (Some card, Some Prof) that some
+ * player disproved the guess with or returns (None, None) if the guess is not
+ * disproved. *)
+let rec help_disprove players state guess=
     match players with
-    |[]   -> None
-    |h::t -> User.disprove guess h else help_disprove t guess
+    |[]   -> (None, None)
+    |h::t ->(match (List.assoc h state.dictionary) with
+            |`Ai    -> let proof= ai_disprove (character_to_ai h state.ais) guess in
+                if proof=None then help_disprove t state guess else (proof, Some h)
+            |`User  -> let proof= User.user_disprove state guess in
+                if proof=None then help_disprove t state guess else (proof, Some h)
+            |`No    -> help_disprove t state guess
+              )
+
+(*[update_possible] returns the updated list of possible cards. If a card was
+ * not able to be disproved and the ai knows that it doesn't possess the card,
+ * then all other cards of that type (building/prof/language) are removed from
+ * the possible cards. If it was not able to be disproved and the ai does
+ * possess it, it just returns the old list of possible cards.*)
+let update_possible ai c =
+  if (List.mem c ai.hand) then ai.possible_cards else if (int_of_card c)
+
 
 (*[update_ai_not_disproved ai guess] updates the [ai] based on the fact that
-[guess] was disproved. Returns updates ai.*)
+[guess] was disproved. Returns updated ai.*)
  (*if not disproved, add to past guesses. Get rid of all other cards
-              of that type in ai.possible_cards. check if we can make an
-            accustaion. If so, we make it, if not, ai ends its turn and the game
-          continues*)
+              of that type in ai.possible_cards.*)
 let update_ai_not_disproved ai guess=
-let new_possible =
-      (**)
-{
-  character =ai.character;
-  hand=ai.hand;
-  was_moved=ai.was_moved;
-  is_in_game=ai.is_in_game;
-  difficulty=ai.difficulty;
-  destination=ai.destination;
-  known_cards=ai.known_cards;
-  possible_cards=ai.possible_cards;
-  past_guesses=ai.past_guesses^(guess, ai.character, None)
-  (*(case_file * prof * (prof option)) list;*)
-}
+print_endline "No one was able to disprove the AI.";
+{ai with past_guesses=(guess, ai.character, None)::ai.past_guesses;
+          possible_cards=(*TODO*))}
 
 
 (*[update_ai_disproved ai guess player] takes the card [c] that was returned by
 [player] who disproved [guess] and updates [ai].*)
              (*if disproved, add card to known cards. Add this guess to
-             past guesses. Check if we want to make an accusation
-              - if so , make it, else, game continues and
-           this turn ends *)
-let update_ai_disproved ai c guess player=(*TODO*)
+             past guesses.  *)
+let update_ai_disproved ai c guess player=
+print_endline "The AI's card has been disproved!";
+{ai with past_guesses=(guess, ai.character, player)::ai.past_guesses;
+       known_cards=c::ai.known_cards}
 
+(*;replace_ai_with_new new_ai ai_list] returns an updated list of ais, replacing
+the old ai with this new one.*)
+let rec replace_ai_with_new new_ai ai_list =
+  match ai_list with
+  |[]-> failwith "not an ai"
+  |h::t-> if h.character=new_ai.character then new_ai::t
+          else h::replace_ai_with_new new_ai t
 
 (* [step state p] peforms a turn for player [p] (an AI player). This involves:
  *   - defining and setting goals by processing knowledge from suggestions and
@@ -184,8 +204,8 @@ let update_ai_disproved ai c guess player=(*TODO*)
  *   - forming and making suggestions/accusations
  * Returns: an updated game state.
  *)
-let step state ai =
-  let moves = User.roll_two_dice () in
+let ai_step state ai =
+  let moves = Data.roll_two_dice () in
   if ai.is_in_game then (
     if Gmap.is_in_building state.map ai.character then
       (Gmap.leave_building state.map 0);
@@ -199,20 +219,39 @@ let step state ai =
             |_    -> failwith "impossible"
           );
             if in_building then
-              (match help_disprove players (make_suggestion dest ai) with
-                |None   ->
-                |Some c ->
-              )
-             (*call the users in turn to try and disprove the suggestion, if no
-             one can, then it'll return None*)
-            (*TODO: replace ai with updated ai*)
-           else if easy_want_to_accuse then make_accusation state ai else None;
-
+              let players =
+              (match ai.character with
+                |Prof "Bracy"   -> [Prof "Clarkson"; Prof "Fan";
+                                Prof "Gries"; Prof "Halpern"; Prof "White"]
+                |Prof "Clarkson"->[Prof "Fan"; Prof "Gries"; Prof "Halpern";
+                                Prof "White"; Prof "Bracy"]
+                |Prof "Fan"     -> [Prof "Gries"; Prof "Halpern";
+                                Prof "White"; Prof "Bracy"; Prof "Clarkson"]
+                |Prof "Gries"   ->[Prof "Halpern";Prof "White"; Prof "Bracy";
+                                Prof "Clarkson"; Prof "Fan"]
+                |Prof "Halpern" ->[Prof "White"; Prof "Bracy";Prof "Clarkson";
+                                Prof "Fan"; Prof "Gries"]
+                |Prof "White"   ->[Prof "Bracy";Prof "Clarkson";
+                                Prof "Fan"; Prof "Gries"; Prof "Halpern"]
+            )
+              in
+              let guess = make_suggestion dest ai in
+              let new_ai =
+              (match help_disprove players state guess with
+                |(None, None)   -> update_ai_not_disproved ai guess
+                |(Some c, Some p) -> update_ai_disproved ai c guess p
+              ) in
+              let new_ai_list=replace_ai_with_new new_ai state.ais in
+              {state with counter=state.counter+1;
+                ais = new_ai_list
+              }
+              else if easy_want_to_accuse then make_accusation state ai else None
+;
 
   |Medium-> failwith "unimplemented"
   |Hard->failwith "unimplemented"
 );
-{
+(*{
   counter=state.counter+1;
   game_complete= game_complete;
   map=state.map;
@@ -220,7 +259,7 @@ let step state ai =
   ais=state.ais;
   fact_file=state.fact_file;
   dictionary=state.dictionary;
-}
+}*)
 else
   state
 
@@ -237,7 +276,7 @@ let rec easy_helper_disprove hand guess = match hand with
  * Returns: [Some c] where [c] is a card that [ai] can reveal. Or, if [ai] has
  * none of the cards in [guess], then it will return [None].
  *)
-let disprove ai (guess: case_file) =
+let ai_disprove ai (guess: case_file) =
   match ai.difficulty with
   |Easy   -> easy_helper_reveal ai.hand guess
   |Medium ->failwith "unimplemented"
@@ -245,3 +284,5 @@ let disprove ai (guess: case_file) =
    show that one so we give the other players as little information as possible.
  *)
     failwith "unimplemented"
+
+
