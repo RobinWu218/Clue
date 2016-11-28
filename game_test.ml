d4ce4e3869017054c2bd9b17e188e9a2dbe6266c
Karen Fang <kf288@cornell.edu>
2016-11-27 23:49:02 -0500

    commented out testing

--- game_test.ml
+++ game_test.ml
@@ -4,29 +4,11 @@ open Ai
 open Data
 
 
-let ai1 ={
-  character ="White";
-  hand=[];
-  was_moved=false;
-  is_in_game=true;
-  difficulty=Easy;
-  destination=None;
-  known_cards=[];
-  possible_cards=[];
-  past_guesses=[]
-  (*(case_file * prof * (prof option)) list;*)
-}
 
-let ai_tests = [
-
-"init"  >:: (fun _ -> assert_equal (ai1)
-    (init "White" Easy []));
-
-]
-
-let ai_test_suite =
+(*let ai_test_suite =
   "Clue Tests"
   >::: tests
 
 
 let _ = run_test_tt_main test_suite
+*)
