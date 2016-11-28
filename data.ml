8cfa6162a1b70cbdecf5a339347fcf31a334fa96
Alice Chen <alicexinyun@163.com>
2016-11-28 00:02:16 -0500

    Cleaned up data.ml and data.mli

--- data.ml
+++ data.ml
@@ -218,30 +218,3 @@ type state = {
   ais:           ai list;
   past_guesses:  (case_file * prof * (prof option)) list;
 }
-
-(* [assign_was_moved s p b] assigns bool [b] to the [was_moved] field of
- * whoever playing the character of prof [p] in state [s]. If no one plays that
- * character, then [s] is simply unchanged. *)
-let assign_was_moved (s:state) (p:prof) (b:bool) : state =
-  match List.assoc p dictionary with
-  | `AI -> 
-      let newais = List.map 
-        (fun a -> if a.character = p 
-                  then {a with was_moved = b} 
-                  else a) s.ais in
-        {s with ais = newais}
-  | `User -> 
-      let newuser = {s.user with was_moved = b} in
-        {s with user = newuser}
-  | `No -> s
-
-(* [roll_two_dice ()] simulates rolling two dice, prints the results, and 
- * returns the sum. *)
-let roll_two_dice () : int =
-  let d1 = 1 + Random.int 5 in
-  let d2 = 1 + Random.int 5 in
-  let sum = d1 + d2 in
-    print_endline "Rolling two dice...";
-    Printf.printf "Die 1: %d\n" d1;
-    Printf.printf "Die 2: %d\n" d2;
-    sum
--- data.mli
+++ data.mli
@@ -1,28 +1,41 @@
-(* [InvalidLocation] is an exception raised by methods in the Map module *)
+(* [InvalidLocation] is raised by functions in the Map module. *)
 exception InvalidLocation
+
+(* [InvalidOperation] is raised by functions in the Map module. *)
 exception InvalidOperation
 
-(* [Difficulty] are variants describing the different levels of AI play. *)
+(* [difficulty] represents the difficulty level of each AI player. *)
 type difficulty = Easy | Medium | Expert
 
-(* professor who started the virus*)
+(* [prof] represents one of the six professors in game. *)
 type prof = string
 
-(* building in where the virus started*)
+(* [building] represents one of the nine buildings in game. *)
 type building = string
 
-(* language that the perpetrator used*)
+(* [language] represents one of the six languages in game. *)
 type language = string
 
-(* card is the type that player can show to prove others' suggestions.
- * It can be a card describing a prof or a building or a language.*)
+(* [card] represents a professor or a building or a language. *)
 type card =
   | Prof of prof
   | Building of building
   | Language of language
 
-(*The hand of what cards a person currently possesses.*)
+(* [hand] represents the cards that a player has. *)
 type hand = card list
+
+(* [prof_of_int i] is the prof corresponding to an integer from 0 to 5. *)
+val prof_of_int : int -> prof
+
+(* [int_of_prof i] is the integer corresponding to a prof. *)
+val int_of_prof : prof -> int
+
+(* [building_of_int i] is the integer corresponding to a building. *)
+val building_of_int : int -> building
+
+(* [lang_of_int i] is the integer corresponding to a language. *)
+val lang_of_int : int -> language
 
 (* [card_of_int i] is the card representation of an integer from 0 to 20. *)
 val card_of_int : int -> card
@@ -33,67 +46,78 @@ val int_of_card : card -> int
 (* [string_of_card c] is the string representation of a card. *)
 val string_of_card : card -> string
 
-(* case_file is the type defining who made the virus in which building with 
- * what kind of programming language.*)
+(* [lst_to_prof_lst lst] is a prof list corresponding to int list [lst]. *)
+val lst_to_prof_lst : string list -> prof list
+
+(* [prof_lst_to_int_lst lst] is an int list corresponding to prof list 
+ * [lst]. *)
+val prof_lst_to_int_lst : prof list -> int list
+
+(* [int_lst_to_card_lst lst] is a card list corresponding to int list [lst]. *)
+val int_lst_to_card_lst : int list -> card list
+
+(* [card_lst_to_int_lst lst] is an int list corresponding to card list [lst]. *)
+val card_lst_to_int_lst : card list -> int list
+
+(* [case_file] represents the fact file, a suggestion, or an accusation, which 
+ * includes information of who started the virus in which building with which 
+ * programming language. *)
 type case_file = {who: prof; where: building; with_what: language}
 
-(* integer pair representing a (row, column) on the map. *)
+(* [coord] represents a (row, column) coordinate on the map. *)
 type coord = int * int
 
-(* map is the type that holds information about the map. *)
+(* [dir] represents the direction and number of steps of a player's movement 
+ * on the map. *)
+type dir = Up of int | Down of int | Left of int | Right of int
+
+(* [map] stores information about the map. *)
 type map = {
-  num_rows:      int;
-  num_cols:      int;
-  map_values:    string option array array;
-  exits:        (building * ((int * coord) list)) list; 
-             (* ("Gates",   [ (1, (0,0)); (2,(5,5)) ] *)
-  buildings:     building list;
-  in_building:  (prof * building)list;
-  location:     (prof * coord) list;
-  waiting_spots:(building * (coord list)) list;
-  secrets:      (building * building) list;
+  num_rows:       int;
+  num_cols:       int;
+  exits:         (building * ((int * coord) list)) list; 
+       (* e.g., [("Gates",   [(1,    (0,0)); 
+                             (2,    (5,5))     ])     ] *)
+  buildings:      building list;
+  waiting_spots: (building * (coord list)) list;
+  secrets:       (building * building) list;
+  (* Below are fields that can change throughout the game. *)
+  map_values:     string option array array;
+  in_building:   (prof * building)list;
+  location:      (prof * coord) list;
 }
 
-(* user stores the information about the user's character, number of turns,
- * specific location and the language that s/he uses.*)
+(* [user] stores information about the user. *)
 type user = {
   character: prof;
   hand:      hand;
+  (* Below is a field that can change throughout the game. *)
   was_moved: bool;
 }
 
-(* ai and player are almost the same except for that ai also has a list of
- * case_file that he obtains*)
+(* [ai] stores information about an ai. *)
 type ai = {
-  character :   prof;
-  hand:         hand;
-  was_moved:    bool;
-  is_in_game:   bool;
-  difficulty:   difficulty;
-  destination:  coord option;
-  known_cards:  card list;
+  character :     prof;
+  hand:           hand;
+  difficulty:     difficulty;
+  (* Below are fields that can change throughout the game. *)
+  was_moved:      bool;
+  is_in_game:     bool;
+  destination:    coord option;
+  known_cards:    card list;
   possible_cards: card list;
+}
+
+(* [state] stores information about the entire game, including user's and ais'
+ * information. *)
+type state = {
+  fact_file:     case_file;
+  dictionary:    (prof * [ `AI | `User | `No ]) list;
+  (* Below are fields that can change throughout the game. *)
+  game_complete: bool;
+  counter:       int;
+  map:           map;
+  user:          user;
+  ais:           ai list;
   past_guesses:  (case_file * prof * (prof option)) list;
 }
-
-(* state is the type specifying the currect map situation and player's and ais' information.
- * Also, it includes a fact_file which was initiated at the init phase of the game.*)
-type state = {
-  counter:  int;
-  game_complete: bool;
-  map:      map;
-  user:     user;
-  ais:      ai list;
-  fact_file:   case_file;
-  dictionary: (prof * [ `AI | `User | `No ]) list;
-}
-
-(* [assign_was_moved s p b] assigns bool [b] to the [was_moved] field of
- * whoever playing the character of prof [p] in state [s]. If no one plays that
- * character, then [s] is simply unchanged. *)
-val assign_was_moved : state -> prof -> bool -> state
-
-(* [roll_two_dice ()] simulates rolling two dice, prints the results, and 
- * returns the sum. *)
-val roll_two_dice : unit -> int
-
