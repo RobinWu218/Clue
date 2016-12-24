57ae0df7973620a46063d3c9440074bc1737c939
RobinWu218 <robinwuzy@gmail.com>
2016-12-24 15:26:33 +0800

    Added utility functions

---
+++ utils.ml
@@ -0,0 +1,13 @@
+let nth = List.nth
+
+let hd = List.hd
+
+let tl = List.tl
+
+let assoc = List.assoc
+
+let find = List.find
+
+let length = List.length
+
+let mem = List.mem
---
+++ utils.mli
@@ -0,0 +1,13 @@
+val nth : 'a list -> int -> 'a
+
+val hd : 'a list -> 'a
+
+val tl : 'a list -> 'a list
+
+val assoc : 'a -> ('a * 'b) list -> 'b
+
+val find : ('a -> bool) -> 'a list -> 'a
+
+val length : 'a list -> int
+
+val mem : 'a -> 'a list -> bool
